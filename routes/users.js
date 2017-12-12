ac5c0abac2c4725b48904ee0333b6d703d38e1a7
yux357206 <357206176@qq.com>
2017-12-12 17:09:39 +0800

    1.0.0

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('users', {
+        title: 'Express & ejs',
+        content : 'hello world'
+    });
 });
 
 module.exports = router;
